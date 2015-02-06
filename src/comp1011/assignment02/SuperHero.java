f8d4a4ee2aa91c3b758bddd92e6102afacf93922
JustinDL <justin_d_lucas@hotmail.com>
2015-02-06 16:16:50 -0500

    SuperHero subclass, finished constructor.

---
+++ src/comp1011/assignment02/SuperHero.java
@@ -0,0 +1,25 @@
+
+package comp1011.assignment02;
+
+/**
+ *
+ * @author Justin
+ * February 6th 2015
+ * Description: Superhero subclass of the Hero superclass
+ */
+public class SuperHero extends Hero {
+    
+    
+    //Constructor +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public SuperHero (String name){
+        super(name);
+        
+        generateRandomPowers();
+    }
+    
+    //Private methods +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    
+    
+    
+    
+}
